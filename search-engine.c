301a71f61b5ace866a90e853533b904428ec1c90
tysonwil <tysonwil@gmail.com>
2013-05-05 22:30:17 -0500

    threading probs

--- search-engine.c
+++ search-engine.c
@@ -1,36 +1,38 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #include "index.h"
 
 
-
-typedef struct Container{
-	char file_name[512];
-	int f_size;
-	char word[512];
-	int w_size;
-	int line;
-}Container;
-
-
-Container pending_words[10];    // Words to be indexed along with metadata
-char * scan_buffer[10000];      // Producer/consumer queue
-int scan_count = 0;
+struct parameter {
+    char test[5];
+};
 
 char argc_error[83] = "Invalid amount of args!\nUsage: search-engine [indexer_thread_amount] [file-list]\n\n\0";
 char unable_to_open[34] = "Invalid file or unable to open!\n\n\0";
+char mutex_one_error[28] = "Semaphore mutex_one error!\n";
+char full_error[30] = "Semaphore full_buffer error!\n";
+char empty_error[31] = "Semaphore empty_buffer error!\n";
+
+char scan_buffer[10][512];
+
+sem_t mutex_one;
+sem_t full_buffer;
+sem_t empty_buffer;
+int finished_activations;
+
+int buffer_amount;
+FILE * file_list;
+
+void startScanning();
+void startIndexing();
 
 
-add_to_buffer(char * filename) {
-    scan_buffer[scan_count] = malloc((strlen(filename)));
-    scan_buffer[scan_count] = filename;
-    printf("%s",scan_buffer[scan_count]);
-    ++scan_count;
-}
 
 /*
  * Activates a single (or multiple) thread by passing in the following parameters.
@@ -40,37 +42,121 @@ add_to_buffer(char * filename) {
  * 		  	void 	function_name		: the function to activate
  *			struct	parameter			: a struct of all parameter the specific function needs
  *
- * possible bugs: parameters may not have been written with correct pointers (& or *) 
+ * possible bugs: parameters may not have been written with correct pointers (& or *)
  * will fix when we test
  */
-activate_Multi_Threader(int number_of_threads, void function_name, struct parameter){
+void activate_Multi_Threader(int number_of_threads, void * function_name, struct parameter param) {
 	pthread_t tidp;
-	int i;
-	for(i = 0; i < number_of_threads; i++){
-	//int  pthread_create(pthread_t  *  thread, pthread_attr_t * attr, void *(*start_routine)(void *), void * arg);
-		thread0 = pthread_create(&tidp, NULL, &function_name, (void *) &parameter);
-		if(thread0 != 0) printf("\ncan't create thread\n");
+	int i, thread0;
+	for (i = 0; i < number_of_threads; i++) {
+        //int  pthread_create(pthread_t  *  thread, pthread_attr_t * attr, void *(*start_routine)(void *), void * arg);
+		thread0 = pthread_create(&tidp, NULL, function_name, (void *) &param);
+		if (thread0 != 0) printf("\ncan't create thread\n");
 	}
-	
-	pthread_join(tidp, NULL); 
-
+    if(finished_activations)
+        pthread_join(tidp, NULL);
 }
-
 //NOTE NOTE NOTE
 //in order to pass in a method to a threader, YOU NEED TO name your method in this manner
 //add_to_buffer(char * filename)  needs to be
 //void* add_to_buffer(char * filename)
 
+void addToBuffer(char * file_name, int buffer_amount)
+{
+    //if (file_name[strlen(file_name) - 1] == '\n') {
+      //  file_name[strlen(file_name) - 1] == '\0';
+    //}
+    
+    //char * dest;
+    strncpy(scan_buffer[buffer_amount], file_name, sizeof(file_name)-1);
+    // USE STRCOPY WHEN INSERTING INTO SCAN_BUFFER
+    strcpy(scan_buffer[buffer_amount], file_name);
+	printf("%s", scan_buffer[buffer_amount]);
+}
 
-int main(int argc, char * argv[]) {
+void removeFromBuffer(int pos)
+{
+    char * file_name;
+    FILE * file;
+    char buffer[512];
+    int line_number;
+    
+    file_name = scan_buffer[pos];
+    
+    printf("File to be indexed: %s", file_name);
+    if ((file = fopen(file_name, "r")) == NULL) {
+        printf("Wasn't able to open: %s", file_name);
+        return;
+    }
+    while (!feof(file)) {
+        line_number = 0;
+        char * word;
+        char * saveptr;
+        fgets(buffer, 512, file);
+        word = strtok_r(buffer, " \n\t-_!@#$%^&*()_+=,./<>?", &saveptr);
+        while (word != NULL) {
+            insert_into_index(word, file_name, line_number);
+            word = strtok_r(NULL, " \n\t-_!@#$%^&*()_+=,./<>?",&saveptr);
+        }
+        line_number = line_number+1;
+    }
+    fclose(file);
+}
 
+void startScanning()
+{
+    char buffer[512];
+    int i;
+    
+    while(!feof(file_list)) {
+		sem_wait(&empty_buffer);
+		sem_wait(&mutex_one);
+        
+		buffer_amount = 0;
+        for (buffer_amount = 0; buffer_amount < 10; buffer_amount++) {
+            if (fgets(buffer, 512, file_list) != NULL) {
+                addToBuffer(buffer, buffer_amount);
+            }
+        }
+		sem_post(&mutex_one);
+		sem_post(&full_buffer);
+        
+        for (i=0; i < 10; i++) {
+            printf("scan_buffer[%d]: %s",i,scan_buffer[i]);
+        }
+	}
+     printf("FINISH SCANNING\n");
+    
+	fclose(file_list);
+}
+
+void startIndexing()
+{
+    int i;
+    
+    printf("Waiting to index\n");
+    sem_wait(&full_buffer);
+    sem_wait(&mutex_one);
+    printf("Begin to index\n");
+    
+    // Implement whatever algo for multiple indexing threads
+    // this one is for one thread, so it is linear
+    for (i=0; i < 10; i++) {
+        removeFromBuffer(i);
+    }
+    
+    sem_post(&mutex_one);
+    sem_post(&empty_buffer);
+}
+
+int main(int argc, char * argv[])
+{
 	// Let's try to guarantee proper usage
 	if (argc != 3) {
 		write(STDERR_FILENO, argc_error, strlen(argc_error));
 		exit(1);
 	}
-
-	FILE * file_list;
+    
 	file_list = fopen(argv[2],"r");
 	
 	// If file is invalid or isn't loaded for some reason, there is no point of moving on
@@ -81,23 +167,28 @@ int main(int argc, char * argv[]) {
 	
 	// How many threads will be indexing
 	int indexer_amount = atoi(argv[1]);
-
-	//BUFFER SIZE?
-	char buffer[512];
-	int line_number;
-	char * word;
-	char * save_ptr;
-
-    // SCANNER
-	// Read in file names from the list of files a.k.a. file_list
-	while(fgets(buffer, 512, file_list) != NULL) {
-        add_to_buffer(buffer);
+    
+    
+	//printf("BEFORE INIT\n");
+	if(sem_init(&mutex_one, 0, 1) != 0) {
+		write(STDERR_FILENO, mutex_one_error, strlen(mutex_one_error));
+		exit(1);
 	}
+	if(sem_init(&full_buffer, 0, 0) != 0) {
+		write(STDERR_FILENO, full_error, strlen(full_error));
+		exit(1);
+	}
+	if(sem_init(&empty_buffer, 0, 1) != 0) {
+		write(STDERR_FILENO, empty_error, strlen(empty_error));
+		exit(1);
+	}
+    finished_activations = 0;
     
-    // INDEXER
+    struct parameter param;
+    activate_Multi_Threader(indexer_amount, startIndexing, param);
+    activate_Multi_Threader(1, startScanning, param);
+    finished_activations = 1;
+    //activate_Multi_Threader(1, SEARCH, param);
     
-
-	fclose(file_list);
-
 	return 0;
 }
